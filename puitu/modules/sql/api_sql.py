cb5ea08309afad98ea61475794bb9407242c8486
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-07-24 19:40:22 +0530

    Add files via upload

---
+++ puitu/modules/sql/api_sql.py
@@ -0,0 +1,67 @@
+#  OpenGM - Powerful  Telegram group managment bot
+#  Copyright (C) 2017 - 2019 Paul Larsen
+#  Copyright (C) 2019 - 2023 Nicky Lalrochhara
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+from sqlalchemy import Column, UnicodeText, Integer, BigInteger
+from puitu.modules.sql import SESSION, BASE
+
+
+class Api(BASE):
+    __tablename__ = "api"
+    user_id = Column(BigInteger, primary_key=True)
+    key = Column(UnicodeText)
+
+    def __init__(self, user_id, key=None):
+        self.user_id = user_id
+        self.key = key
+
+    def __repr__(self):
+        return "<User {} ({})>".format(self.username, self.user_id)
+
+
+Api.__table__.create(checkfirst=True)
+
+
+def set_key(user_id, key):
+    user = SESSION.query(Api).get(user_id)
+    if not user:
+        user = Api(user_id, key)
+        SESSION.add(user)
+        SESSION.flush()
+    else:
+        user.key = key
+    SESSION.commit()
+
+
+def verify_key(key):
+    result = SESSION.query(Api).filter(Api.key == str(key)).scalar()
+    ret = None
+    if result:
+        ret = result.user_id
+    SESSION.close()
+    return ret
+
+
+def get_key(user_id):
+    key = SESSION.query(Api).get(str(user_id))
+    ret = "null"
+    if key:
+        ret = key.key
+
+    SESSION.close()
+    return ret
---
+++ puitu/modules/sql/lang_sql.py
@@ -0,0 +1,77 @@
+#  OpenGM - Powerful  Telegram group managment bot
+#  Copyright (C) 2017 - 2019 Paul Larsen
+#  Copyright (C) 2019 - 2023 Nicky Lalrochhara
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import threading
+
+from sqlalchemy import Column, String, UnicodeText, func, distinct
+
+from puitu.modules.sql import SESSION, BASE
+from puitu import DEFAULT_LANG
+
+
+class Lang(BASE):
+    __tablename__ = "lang"
+    chat_id = Column(String(14), primary_key=True)
+    lang = Column(UnicodeText, default="")
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+    def __repr__(self):
+        return "<Chat {} Language: {}>".format(self.chat_id, self.lang)
+
+
+Lang.__table__.create(checkfirst=True)
+
+INSERTION_LOCK = threading.RLock()
+
+
+def set_lang(chat_id, lang_text):
+    with INSERTION_LOCK:
+        l = SESSION.query(Lang).get(str(chat_id))
+        if not l:
+            l = Lang(str(chat_id))
+        l.lang = lang_text
+
+        SESSION.add(l)
+        SESSION.commit()
+
+
+def get_lang(chat_id):
+    lang = SESSION.query(Lang).get(str(chat_id))
+    ret = DEFAULT_LANG
+    if lang:
+        ret = lang.lang
+
+    SESSION.close()
+    return ret
+
+
+def num_chats():
+    try:
+        return SESSION.query(func.count(distinct(Lang.chat_id))).scalar()
+    finally:
+        SESSION.close()
+
+
+def migrate_chat(old_chat_id, new_chat_id):
+    with INSERTION_LOCK:
+        chat = SESSION.query(Lang).get(str(old_chat_id))
+        if chat:
+            chat.chat_id = str(new_chat_id)
+        SESSION.commit()
